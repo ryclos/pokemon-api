4ce679b543b53c3e7f209f993a120b203f5b519e
AsKhaN <khraken@outlook.fr>
2021-10-12 19:20:34 -1000

    Correct hash beforeCreate -> bcrypt

--- src/db/controller/userController.js
+++ src/db/controller/userController.js
@@ -1,92 +1,104 @@
-const User = require("../models/userModel");
+const User = require('../models/userModel');
 
 const getAllUser = async (req, res) => {
-    try {
-        const users = await User.findAll();
-        res.status(200).json(users);
-        if (users === null) { return res.status(404).json(users); }
-    } catch (e) {
-        res.status(500).json(e);
-    };
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+    if (users === null) {
+      return res.status(404).json(users);
+    }
+  } catch (e) {
+    res.status(500).json(e);
+  }
 };
 
 const getUserByPk = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const user = await User.findByPk(id);
-        if (user === null) { return res.status(404).json(user); }
-        res.status(200).json(user);
-    } catch (e) {
-        res.status(500).json(e);
+  try {
+    const id = req.params.id;
+    const user = await User.findByPk(id);
+    if (user === null) {
+      return res.status(404).json(user);
     }
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 };
 
 const registerUser = async (req, res) => {
-    try {
-        const body = req.body;
-        const hash = User.beforeCreate
-        const user = await hash.create(body);
-        if (user === null) { return res.status(404).json(user); }
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json(error);
+  try {
+    const userInput = req.body;
+    const user = await User.create({
+      username: userInput.username,
+      email: userInput.email,
+      password: userInput.password,
+    });
+
+    if (user === null) {
+      return res.status(404).json(user);
     }
+    res.status(200).json(jane);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 const loginUser = async (req, res) => {
-    try {
-        const body = req.body.username;
-        const user = await User.findOne(body);
-        const isPasswordValid = req.hooks.validPassword()
-        if (isPasswordValid) {
-            const message = `L'utilisateur a été connecté avec succès`;
-            console.log(isPasswordValid)
-            return res.json({message, data: user})
-        }
-    } catch (error) {
-        res.status(500).json(error);
+  try {
+    const body = req.body.username;
+    const user = await User.findOne(body);
+    const isPasswordValid = req.hooks.validPassword();
+    if (isPasswordValid) {
+      const message = `L'utilisateur a été connecté avec succès`;
+      console.log(isPasswordValid);
+      return res.json({ message, data: user });
     }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 const updateUser = async (req, res) => {
-    try {
-        const userId = req.params.id;
-        const userBody = req.body
-        const findUser = await User.findByPk(userId);
-        if (findUser === null) {
-            const message = `Le l'utilisateur ${userBody.username} demandé n'existe pas. Réessayer avec un autre identifiant.`
-            return res.status(404).json({message})
-        }
-        const updateUser = (await findUser).update(userBody);
-        if (updateUser === null) {
-            const message = `Le pokemon demandé n'a pas pu être modifié.`
-            return res.status(400).json({message})
-        }
-        res.status(200).json(updateUser)
-    } catch (e) {
-        res.status(500).json(e);
+  try {
+    const userId = req.params.id;
+    const userBody = req.body;
+    const findUser = await User.findByPk(userId);
+    if (findUser === null) {
+      const message = `Le l'utilisateur ${userBody.username} demandé n'existe pas. Réessayer avec un autre identifiant.`;
+      return res.status(404).json({ message });
     }
+    const updateUser = (await findUser).update(userBody);
+    if (updateUser === null) {
+      const message = `Le pokemon demandé n'a pas pu être modifié.`;
+      return res.status(400).json({ message });
+    }
+    res.status(200).json(updateUser);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 };
 
 const deleteUser = async (req, res) => {
-    try {
-        const userId = req.params.id
-        const userBody = req.body
-        const findUser = await User.findByPk(userId)
-        if (userId === null) { return res.status(404).json(userId); }
-        await findUser.destroy(userBody)
-        const msg = `Vous avez bien supprimé ${userBody}`
-        res.status(200).json({ msg })
-    } catch (e) {
-        res.status(500).json(e)
+  try {
+    const userId = req.params.id;
+    const userBody = req.body;
+    const findUser = await User.findByPk(userId);
+    if (userId === null) {
+      return res.status(404).json(userId);
     }
-}
+    await findUser.destroy(userBody);
+    const msg = `Vous avez bien supprimé ${userBody}`;
+    res.status(200).json({ msg });
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
 
 module.exports = {
-    getAllUser,
-    getUserByPk,
-    registerUser,
-    loginUser,
-    updateUser,
-    deleteUser
-}
+  getAllUser,
+  getUserByPk,
+  registerUser,
+  loginUser,
+  updateUser,
+  deleteUser,
+};
--- src/db/models/userModel.js
+++ src/db/models/userModel.js
@@ -1,65 +1,60 @@
-const { sequelize } = require('../index')
-const { DataTypes } = require('sequelize')
-const bcrypt = require('bcrypt')
+const { sequelize } = require('../index');
+const { DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 
-const { STRING } = DataTypes
+const { STRING } = DataTypes;
 
-const User = sequelize.define('user', {
+const User = sequelize.define(
+  'user',
+  {
     //ID auto
-    username:{
-        type: STRING,
-        unique: {
-            args: true,
-            msg: 'Ce username existe déjà'
-        },
+    username: {
+      type: STRING,
+      unique: {
+        args: true,
+        msg: 'Ce username existe déjà',
+      },
     },
     email: {
-        type: STRING,
-        allowNull: false,
-        unique: {
-            args: true,
-            msg: 'Ce username existe déjà'
-        },
+      type: STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: 'Ce username existe déjà',
+      },
     },
     password: {
-        type: STRING
-    }
-
-},
-    {
-        freezeTableName: true,
-        timestamps: false
+      type: STRING,
     },
-    {
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  },
+  {
     hooks: {
-        beforeCreate: async (user) => {
-            if (user.password) {
-                const salt = await bcrypt.genSaltSync(10, 'a');
-                user.password = bcrypt.hashSync(user.password, salt);
-            }
-        },
-        beforeUpdate: async (user) => {
-            if (user.password) {
-                const salt = await bcrypt.genSaltSync(10, 'a');
-                user.password = bcrypt.hashSync(user.password, salt);
-            }
-        }
+      beforeCreate: async (user) => {
+        const userPassword = user.getDataValue('password').toString();
+        const salt = await bcrypt.genSalt(10, 'a');
+        const userPasswordHashed = bcrypt.hashSync(userPassword, salt);
+
+        user.setDataValue('password', userPasswordHashed);
+      },
     },
     instanceMethods: {
-        validPassword: async (password) => {
-            try {
-                const user = this
-                return await bcrypt.compareSync(password, user.password);
-            } catch (e) {
-                throw e
-            }
+      validPassword: async (password) => {
+        try {
+          const user = this;
+          return await bcrypt.compareSync(password, user.password);
+        } catch (e) {
+          throw e;
         }
-    }
-});
+      },
+    },
+  }
+);
 User.prototype.validPassword = async (password, hash) => {
-    return await bcrypt.compareSync(password, hash);
-}
+  return await bcrypt.compareSync(password, hash);
+};
 
-module.exports = User
-
-
+module.exports = User;
